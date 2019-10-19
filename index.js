e08b7fb58c24d9cdf03ce431bc5e10febf90dbff
Halisson Bruno <halissonvit@gmail.com>
2019-10-19 03:13:33 -0300

    add memory cache

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const path = require("path");
 const https = require("https");
 const cheerio = require("cheerio");
+const cache = require("memory-cache");
 
 const PORT = process.env.PORT || 5000;
 
-const AVAILABILITIES_URL =
+const PROPERTIES_URL =
   "https://venda-imoveis.caixa.gov.br/listaweb/Lista_imoveis_GO.htm?191651097";
 
 const CITIES = ["GOIANIA"];
@@ -35,12 +36,39 @@ const parseURLCell = ($, tr, index) =>
     .find("a")
     .attr("href");
 
+// configure cache middleware
+let memCache = new cache.Cache();
+let cacheMiddleware = duration => {
+  return (req, res, next) => {
+    let key = "__express__" + req.originalUrl || req.url;
+    let cacheContent = memCache.get(key);
+    if (cacheContent) {
+      console.log(`Serving cached content for key: ${key}`);
+      res.send(cacheContent);
+      return;
+    } else {
+      console.log(`Purging cache!`);
+      memCache.clear();
+      res.sendResponse = res.send;
+      res.send = body => {
+        console.log(`Caching content for key: ${key}`);
+        memCache.put(key, body, duration * 1000);
+        res.sendResponse(body);
+      };
+      next();
+    }
+  };
+};
+
 express()
   .get("/", (req, res) => {
-    console.log(`Fetching availabilities from ${AVAILABILITIES_URL}`);
+    return res.send("ok");
+  })
+  .get("/properties", cacheMiddleware(1440), (req, res) => {
+    console.log(`Fetching properties from ${PROPERTIES_URL}`);
 
     https.get(
-      AVAILABILITIES_URL,
+      PROPERTIES_URL,
 
       response => {
         response.setEncoding("binary");
@@ -51,14 +79,13 @@ express()
         });
 
         response.on("end", () => {
-          console.log(body);
+          let $ = cheerio.load(body, { decodeEntities: true });
+          body = null;
 
-          const $ = cheerio.load(body, { decodeEntities: true });
-          const allAvailabilities = $("tr");
-          console.log(`Filtering ${allAvailabilities.length} availabilities`);
+          const allProperties = $("tr");
+          console.log(`Filtering ${allProperties.length} properties`);
 
-          debugger;
-          const availabilities = allAvailabilities
+          const properties = allProperties
             .map(function(i, el) {
               const url = parseURLCell($, this, 0);
               const address = parseTextCell($, this, 1);
@@ -92,18 +119,12 @@ express()
               );
             });
 
-          console.log(
-            "\n\n\n\n\n===================================================="
-          );
-          console.log(`Found ${availabilities.length} availabilities`);
-          console.log(availabilities);
-          console.log(
-            "\n\n\n\n\n===================================================="
-          );
+          $ = null;
+          console.log(`Found ${properties.length} properties`);
 
           return res.json({
-            total: availabilities.length,
-            data: availabilities
+            total: properties.length,
+            data: properties
           });
         });
       }
