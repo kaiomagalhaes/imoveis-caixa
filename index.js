a71f9751562d82552e292186bd030b92e5d631be
Halisson Bruno <halissonvit@gmail.com>
2019-10-27 01:12:36 -0300

    modify app to work in the background and post results to slack

--- index.js
+++ index.js
@@ -3,19 +3,38 @@ const path = require("path");
 const https = require("https");
 const cheerio = require("cheerio");
 const cache = require("memory-cache");
+const { WebClient } = require("@slack/web-api");
+
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
+const SLACK_API_KEY = process.env.SLACK_API_KEY;
+const SLACK_TEAM_ID = process.env.SLACK_CHANNEL_ID;
+const SLACK_CHANNEL_ID = process.env.SLACK_CHANNEL_ID;
+const FETCH_MISSING_INFO =
+  `${process.env.FETCH_MISSING_INFO}`.toLowerCase() == "true";
+const PROGRESS_LOG_INTERVAL_IN_SECONDS = parseInt(
+  process.env.PROGRESS_LOG_INTERVAL_IN_SECONDS || 3
+);
+const CITIES = process.env.CITIES.trim().split(",");
+const NEIGHBORHOODS = process.env.NEIGHBORHOODS.trim().split(",");
+
+if (
+  !SLACK_API_KEY ||
+  !SLACK_CHANNEL_ID ||
+  !SLACK_CHANNEL_ID ||
+  !CITIES ||
+  !NEIGHBORHOODS
+) {
+  throw new Error(
+    "Please set SLACK_API_KEY, SLACK_TEAM_ID, SLACK_CHANNEL_ID, CITIES AND NEIGHBORHOODS!"
+  );
+}
 
 const PROPERTIES_URL =
   "https://venda-imoveis.caixa.gov.br/listaweb/Lista_imoveis_GO.htm?191651097";
 
-const CITIES = ["GOIANIA"];
-const NEIGHBORHOODS = [
-  "ALTO DA GLORIA",
-  "JARDIM GOIAS",
-  "SETOR BUENO",
-  "SETOR MARISTA"
-];
+const slackClient = new WebClient(SLACK_API_KEY);
 
 const parseTextCell = ($, tr, index) =>
   $(
@@ -60,74 +79,147 @@ let cacheMiddleware = duration => {
   };
 };
 
+const postPropertyToSlack = async (property, threadId) => {
+  const text = `\`\`\`${property.description}\`\`\`
+
+  
+*Endereço*: ${property.address} 
+*Bairro*: ${property.neighborhood}
+*Valor Original*: R$ ${property.valuation}
+*Valor Atual*: R$ ${property.amount} (${property.discount}% abaixo do valor original)
+*Situação*: ${property.saleType}
+
+
+<${property.url}>
+  `;
+  console.log("Sending property to slack", text);
+
+  const result = await slackClient.chat.postMessage({
+    text,
+    team: SLACK_TEAM_ID,
+    channel: SLACK_CHANNEL_ID,
+    icon_emoji: ":house_with_garden:",
+    username: "Buscador de Imóveis",
+    thread_ts: threadId
+  });
+
+  // The result contains an identifier for the message, `ts`.
+  console.log(
+    `Successfully sent message ${result.ts} in conversation ${SLACK_CHANNEL_ID}`
+  );
+};
+
+const postPropertiesToSlack = async properties => {
+  const text =
+    properties.length > 0
+      ? `
+  ${":chandler_dance:".repeat(properties.length)}
+  Temos ${properties.length} ${properties.length == 1 ? "imóvel" : "imóveis"} ${
+          properties.length == 1 ? "disponível" : "disponíveis"
+        }!!
+  ${":chandler_dance:".repeat(properties.length)}
+  ------------------
+  Localizações disponíveis: ${[
+    ...new Set(properties.map(({ neighborhood }) => neighborhood))
+  ].join(", ")}
+  ------------------
+  `
+      : "Nenhum imóvel hoje. Sorry :sad_face:";
+
+  const result = await slackClient.chat.postMessage({
+    text,
+    team: SLACK_TEAM_ID,
+    channel: SLACK_CHANNEL_ID,
+    icon_emoji: ":house_with_garden:",
+    username: "Buscador de Imóveis"
+  });
+
+  // The result contains an identifier for the message, `ts`.
+  console.log(
+    `Successfully sent message ${result.ts} in conversation ${SLACK_CHANNEL_ID}`
+  );
+
+  await properties.forEach(property => {
+    postPropertyToSlack(property, result.ts);
+  });
+};
+
+const fetchProperties = async () => {
+  console.log(`Fetching properties from ${PROPERTIES_URL}`);
+
+  return https.get(
+    PROPERTIES_URL,
+
+    response => {
+      response.setEncoding("binary");
+      let body = "";
+      let dateCreated = new Date();
+      let lastUpdated = dateCreated;
+
+      response.on("data", data => {
+        const timeSinceLastUpdate = new Date() - lastUpdated;
+        if (timeSinceLastUpdate > PROGRESS_LOG_INTERVAL_IN_SECONDS * 1000) {
+          lastUpdated = new Date();
+          console.info(
+            "OH jeez! %dms seconds since we started!!!",
+            (lastUpdated - dateCreated) / 1000
+          );
+        }
+        body += data;
+      });
+
+      response.on("end", () => {
+        let $ = cheerio.load(body, { decodeEntities: true });
+        body = null;
+
+        const allProperties = $("tr");
+        console.log(`Filtering ${allProperties.length} properties`);
+
+        const properties = allProperties
+          .map(function(i, el) {
+            return {
+              url: parseURLCell($, this, 0),
+              address: parseTextCell($, this, 1),
+              neighborhood: parseTextCell($, this, 2),
+              description: parseTextCell($, this, 3),
+              amount: parseTextCell($, this, 4),
+              valuation: parseTextCell($, this, 5),
+              discount: parseTextCell($, this, 6),
+              saleType: parseTextCell($, this, 7),
+              city: parseTextCell($, this, 9),
+              state: parseTextCell($, this, 10)
+            };
+          })
+          .get()
+          .filter(function({ city, neighborhood }) {
+            return (
+              CITIES.some(c => c === city) &&
+              ((FETCH_MISSING_INFO && !neighborhood) ||
+                NEIGHBORHOODS.some(n => n === neighborhood))
+            );
+          });
+
+        $ = null;
+
+        console.log(`Found ${properties.length} properties`);
+        postPropertiesToSlack(properties);
+      });
+    }
+  );
+};
+
 express()
   .get("/", (req, res) => {
     return res.send("ok");
   })
-  .get("/properties", cacheMiddleware(1440), (req, res) => {
-    console.log(`Fetching properties from ${PROPERTIES_URL}`);
+  //TODO: change to post
+  .post("/properties", async (req, res) => {
+    console.log(`Started process to fetch properties!`);
 
-    https.get(
-      PROPERTIES_URL,
-
-      response => {
-        response.setEncoding("binary");
-        let body = "";
-
-        response.on("data", data => {
-          body += data;
-        });
-
-        response.on("end", () => {
-          let $ = cheerio.load(body, { decodeEntities: true });
-          body = null;
-
-          const allProperties = $("tr");
-          console.log(`Filtering ${allProperties.length} properties`);
-
-          const properties = allProperties
-            .map(function(i, el) {
-              const url = parseURLCell($, this, 0);
-              const address = parseTextCell($, this, 1);
-              const neighborhood = parseTextCell($, this, 2);
-              const description = parseTextCell($, this, 3);
-              const amount = parseTextCell($, this, 4);
-              const valuation = parseTextCell($, this, 5);
-              const discount = parseTextCell($, this, 6);
-              const saleType = parseTextCell($, this, 7);
-              const city = parseTextCell($, this, 9);
-              const state = parseTextCell($, this, 10);
-
-              return {
-                url,
-                address,
-                neighborhood,
-                description,
-                amount,
-                valuation,
-                discount,
-                saleType,
-                city,
-                state
-              };
-            })
-            .get()
-            .filter(function({ city, neighborhood }) {
-              return (
-                CITIES.some(c => c === city) &&
-                (!neighborhood || NEIGHBORHOODS.some(n => n === neighborhood))
-              );
-            });
-
-          $ = null;
-          console.log(`Found ${properties.length} properties`);
-
-          return res.json({
-            total: properties.length,
-            data: properties
-          });
-        });
-      }
-    );
+    setTimeout(fetchProperties, 0);
+    return res.json({
+      message: "Process started",
+      data: {}
+    });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
